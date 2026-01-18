1d98811fb31e1a08d8e743edc0d1f5ff43a8e732
Jin <kbg08167@naver.com>
2026-01-18 16:37:33 +0900

    Made Repo

---
+++ SeongJin/Solution.java
@@ -0,0 +1,16 @@
+package SeongJin;
+import java.io.*;
+import java.util.*;
+
+public class Solution{
+    public static void main(String[] args) throws IOException  {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc =1; tc<=T; tc++){
+            
+        }
+    }
+}
